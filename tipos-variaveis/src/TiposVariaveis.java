84bd06128577512747d7de73b2af2708ee48700c
Juliana Morais <jujubratz27@gmail.com>
2024-08-07 01:24:35 -0300

    Treino de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String meuNome = "Juliana Morais";
+
+        double salarioMinimo = 2500;
+        float salarioBruto = 10000F;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 2;
+        
+        numero = 4;
+
+        System.out.println(numero);
+        
+        final double VALOR_DE_PI = 3.14;
+
     }
 }
